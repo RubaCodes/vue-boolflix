21d868031af74db2b1475835e2c0f996031081a3
rubacodes <rubacodes@gmail.com>
2022-06-09 11:43:26 +0200

    creato mono componente ricerca milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <BaseMain></BaseMain>
+  </div>
 </template>
 
 <script>
+import BaseMain from './components/BaseMain.vue';
 export default {
   name: 'App',
-  components: {},
+  components: { BaseMain },
 };
 </script>
---
+++ src/components/BaseMain.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="temp">
+    <header class="container mx-auto py-4">
+      <form class="flex justify-center items-center py-4 gap-5">
+        <input
+          type="text"
+          v-model="searchText"
+          placeholder="Inserisci titolo..."
+          class="rounded py-2 px-8 border-2 border-red-500"
+        />
+        <button class="bg-red-500 py-2 px-4 rounded">Cerca ...</button>
+      </form>
+    </header>
+    <main>
+      <ul>
+        <li></li>
+      </ul>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseMain',
+  data() {
+    return {
+      searchText: '',
+    };
+  },
+};
+</script>

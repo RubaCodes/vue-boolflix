74d36c302bab6af6dad3e1323d85cedc0de23dcd
rubacodes <rubacodes@gmail.com>
2022-06-09 12:00:55 +0200

    realizzata milestone 1

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="temp">
     <header class="container mx-auto py-4">
-      <form class="flex justify-center items-center py-4 gap-5">
+      <form
+        @submit.prevent="getMovies"
+        class="flex justify-center items-center py-4 gap-5"
+      >
         <input
           type="text"
           v-model="searchText"
@@ -11,21 +14,49 @@
         <button class="bg-red-500 py-2 px-4 rounded">Cerca ...</button>
       </form>
     </header>
-    <main>
+    <main class="container mx-auto">
       <ul>
-        <li></li>
+        <li v-for="film in films" :key="film.id">
+          <h2>
+            {{ film.title }}
+          </h2>
+          <h3>{{ film.original_title }}</h3>
+          <h4>{{ film.original_language }}</h4>
+          <h4>{{ film.vote_average }}</h4>
+        </li>
       </ul>
     </main>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'BaseMain',
   data() {
     return {
+      apiKey: '7c5108b2d54ed416106260111c03e2d9',
       searchText: '',
+      films: [],
     };
+  },
+  methods: {
+    getMovies() {
+      axios
+        .get('https://api.themoviedb.org/3/search/movie?', {
+          params: {
+            api_key: this.apiKey,
+            query: this.searchText,
+            language: 'it-IT',
+          },
+        })
+        .then((response) => {
+          this.films = response.data.results;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 };
 </script>

327b9e9959097a44d6ca156d3fbfc6ad27ef80ec
rubacodes <rubacodes@gmail.com>
2022-06-10 11:08:40 +0200

    gestione iniziale pagina vuota

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-black py-4 flex items-center justify-between">
     <img
-      src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fmedia-assets.wired.it%2Fphotos%2F615ef4b55ccc3b73fb14d5b2%2Fmaster%2Fpass%2Fwired_placeholder_dummy.png&sp=1654783368Tdd2bc8a4da4d585d116d6b8189c2ed7be5d6dc19256104b4595a85ecd9755b80"
+      src="https://www.mobileworld.it/wp-content/uploads/2016/06/netflix-nuova-icona-800x534.png"
       alt="logo netflix"
       width="80"
     />
--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -1,13 +1,22 @@
 <template>
   <main class="bg-gray-900">
-    <SectionMedia class="container mx-auto"></SectionMedia>
+    <SectionMedia
+      class="container mx-auto"
+      :class="{ 'h-[92vh]': !data.films.length }"
+    ></SectionMedia>
   </main>
 </template>
 
 <script>
 import SectionMedia from './sections/SectionMedia.vue';
+import data from '../data/data.js';
 export default {
   components: { SectionMedia },
   name: 'BaseMain',
+  data() {
+    return {
+      data,
+    };
+  },
 };
 </script>

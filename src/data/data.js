e6cdfc5743c34f19adb23349a708a2d22ddd5d85
rubacodes <rubacodes@gmail.com>
2022-06-12 14:39:35 +0200

    commenti  sui data

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 export default Vue.observable({
+  //testo ricerca serie/film
   searchText: '',
+  //liste dei generi
   movieGenreList: [],
   tvShowsGenreList: [],
+  //listato dei film di tendenza
   trending: [],
+  //listato e serie cercate
   films: [],
   tvShows: [],
+  //risorse per le bandiere
   flagsSource: {
     en: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/640px-Flag_of_the_United_Kingdom.svg.png',
     es: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.svg.png',
@@ -15,4 +20,6 @@ export default Vue.observable({
     globe:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/International_Flag_of_Planet_Earth.svg/800px-International_Flag_of_Planet_Earth.svg.png',
   },
+  //test gestione caricamento pagina
+  processing: true,
 });
